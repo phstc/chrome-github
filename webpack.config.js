767fad31a85d48147e7b513c95696bd079ca3317
Pablo Cantero <pablo@pablocantero.com>
2018-04-07 18:08:13 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+var webpack = require("webpack"),
+    path = require("path"),
+    fileSystem = require("fs"),
+    env = require("./utils/env"),
+    CleanWebpackPlugin = require("clean-webpack-plugin"),
+    CopyWebpackPlugin = require("copy-webpack-plugin")
+
+// load the secrets
+var alias = {};
+
+var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
+
+var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
+
+if (fileSystem.existsSync(secretsPath)) {
+  alias["secrets"] = secretsPath;
+}
+
+var options = {
+  entry: {
+    eventPage: path.join(__dirname, "src", "js", "eventPage.js"),
+    content: path.join(__dirname, "src", "js", "content.js")
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: new RegExp('\.(' + fileExtensions.join('|') + ')$'),
+        loader: "file-loader?name=[name].[ext]",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    alias: alias
+  },
+  plugins: [
+    // clean the build folder
+    new CleanWebpackPlugin(["build"]),
+    // expose and write the allowed env vars on the compiled bundle
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
+    }),
+    new CopyWebpackPlugin([{
+      from: "src/manifest.json",
+      transform: function (content, path) {
+        // generates the manifest file using the package.json informations
+        return Buffer.from(JSON.stringify({
+          description: process.env.npm_package_description,
+          version: process.env.npm_package_version,
+          ...JSON.parse(content.toString())
+        }))
+      }
+    }]),
+    new CopyWebpackPlugin([{ from: "src/img" }])
+  ]
+};
+
+if (env.NODE_ENV === "development") {
+  options.devtool = "cheap-module-eval-source-map";
+}
+
+module.exports = options;
